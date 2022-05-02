f72b14081266284b807e13e50bec22b8c1c1a4e1
Jeff Eaton <7427899+jeffeaton@users.noreply.github.com>
2022-05-02 21:17:39 +0100

    add license and add_one()

---
+++ R/gha.eigen.test-package.R
@@ -0,0 +1,6 @@
+## usethis namespace: start
+#' @useDynLib gha.eigen.test, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @import RcppEigen
+## usethis namespace: end
+NULL
--- src/code.cpp
+++ src/code.cpp
@@ -1,2 +1,14 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+
+//' Add 1
+//'
+//' @param x a number
+//'
+//' @export
+//' 
+// [[Rcpp::export]]
+
+double add_one(const double x) {
+  return(x + 1.0);
+}
